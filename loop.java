294c0ffa8100d882e19539dbf8b4f631dc6de755
Aman Patel <aman17626@gmail.com>
2021-05-08 10:35:39 +0530

    First Commit

---
+++ loop.java
@@ -0,0 +1,11 @@
+
+public class loop {
+    public static void main(String arg[])
+    {
+        for(int i = 65; i <= 90; i++) //for(ch = 65; ch <= 92; ch++)
+        {
+            System.out.print("i = "+char(i));
+        }
+    }
+    
+}
